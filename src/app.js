202c5ccd3281f1629ff120b5f0715234f145ea2c
Dominic Pettifer <sironfoot@gmail.com>
2011-08-09 00:01:09 +0100

    Added 'websocket' package from Worlize/WebSocket-Node and got data sending/receiving

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var net = require('net');
 var crypto = require("crypto");
+var WebSocketServer = require('websocket').server;
 
 var app = module.exports = express.createServer();
 
@@ -15,6 +16,11 @@ Array.prototype.remove = function(e) {
   for (var i = 0; i < this.length; i++)
     if (e == this[i]) return this.splice(i, 1);
 }
+
+var wsServer = new WebSocketServer({
+  httpServer: app,
+  autoAcceptConnections: true
+});
 
 
 // Configuration
@@ -49,108 +55,24 @@ app.get('/', function(req, res){
 
 app.listen(3000);
 
-function pushDataToSockets(element, index, array)
-{
-  element.write(index + ': ' + this);
-}
 
-var clients = [];
+wsServer.on('connect', function(connection) {
+  console.log((new Date()) + ' Connection accepted.');
 
-var socketServer = net.createServer(function(socket) {
-
-  var client = {};
-
-  client.socket = socket;
-  client.handshaked = false;
-
-  clients.push(client);
-
-  console.log(clients.length + ' socket(s)');
-  
-  socket.on('data', function(data) {
-
-    console.log('Incoming data...'); 
-    
-    if(!client.handshaked)
-    {
-      //sockets.forEach(pushDataToSockets, data);
-
-      var dataString = data.toString('binary');
-
-      var lines = dataString.split('\r\n');
-
-      var headers = {};
-
-      lines.forEach(function(element, index, array) {
-        if(element.length > 0 && element.indexOf('GET') !== 0)
-        {
-          var keyValue = element.split(': ');
-          var key = keyValue[0];
-          var value = keyValue[1];
-
-          headers[key] = value;
-        }
-      });
-
-      //console.log(headers);
-
-      var secretKey = headers['Sec-WebSocket-Key'];
-
-      //console.log('The key is: "' + secretKey + '"');
-
-      var returnSecretKey = secretKey + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
-      var sha1 = crypto.createHash("sha1");
-      sha1.update(returnSecretKey);
-
-      var returnHash = sha1.digest('base64');
-
-      //console.log(returnHash);
-
-      var responseHeaders = 'HTTP/1.1 101 Switching Protocols\r\n' +
-                            'Upgrade: websocket\r\n' +
-                            'Connection: Upgrade\r\n' +
-                            'Sec-WebSocket-Accept: ' + returnHash + '\r\n' +
-                            '\r\n';
-                          
-      socket.write(responseHeaders);
-
-      client.handshaked = true;
+  connection.on('message', function(message) {
+    if (message.type == 'utf8') {
+      console.log('Received message: ' + message.utf8Data);
+      connection.sendUTF('You said: "' + message.utf8Data + '"');
     }
-    else
-    {
-      
-
-      console.log(data.toString('base64'));
+    else if (message.type == 'binary') {
+      console.log('Received Binary Message of ' + message.binaryData.length + ' bytes.');
     }
   });
 
-  socket.on('end', function() {
-    console.log('on end');
+  connection.on('close', function(connection) {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
   });
-
-  socket.on('close', function() {
-
-    clients.remove(client);
-
-    console.log('on close');
-  });
-
-  socket.on('error', function() {
-    console.log('on error');
-  });
-
-  //socket.write('hello world');
-
-  //socket.pipe(socket);
-
 });
-
-
-
-
-
-socketServer.listen(8080, 'localhost');
-
 
 
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
